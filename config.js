80c0ab97ad4734315ee6871b9faf6d61fd1bf498
Tony Spiro <tspiro@tonyspiro.com>
2017-04-27 17:20:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,8 @@
 export default {
   bucket: {
     slug: process.env.COSMIC_BUCKET || 'cosmic-js-chat',
-    type_slug: 'messages'
+    type_slug: 'messages',
+    read_key: process.env.COSMIC_READ_KEY,
+    write_key: process.env.COSMIC_WRITE_KEY
   }
 }
